981c6db163185e4eeaf2c1d670655d8c9c009c31
Baruch9353 <baruchminzberg@gmail.com>
2025-06-23 17:46:47 +0300

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,70 @@
+// Q1
+// const num5 = "12345";
+// let num = 0;
+// for(let i = 0; i<num5.length; i++)
+//     {
+//         num += Number (num5[i]);
+//     };
+// console.log(num);
+  
+// Q2
+// for (let i = 1; i <= 5; i++) {
+//     let row = "";
+//     for (let j = 0; j < i; j++) {
+//         row += "*";
+//     }
+//     console.log(row);
+// };
+
+// Q3
+
+// const num1 = 12;
+// const num2 = 7;
+// const num3 = 19;
+// const num4 = 4;
+// const num5 = 16;
+
+// let largest = num1;
+
+// if (num2 > largest) {
+//     largest = num2;
+// }
+// if (num3 > largest) {
+//     largest = num3;
+// }
+// if (num4 > largest) {
+//     largest = num4;
+// }
+// if (num5 > largest) {
+//     largest = num5;
+// }
+// console.log(largest); 
+
+// Q4
+// const num = "123456";
+// console.log(num.length);
+
+// Q5
+// let num = 1234;
+
+// let a = num % 10;
+// let b = Math.floor(num / 10) % 10;         
+// let c = Math.floor(num / 100) % 10;         
+// let d = Math.floor(num / 1000) % 10;       
+
+// const newNum = a * 1000 + b * 100 + c * 10 + d;
+
+// console.log(newNum); 
+
+// Q6
+
+
+
+
+
+
+
+    
+
+
+
